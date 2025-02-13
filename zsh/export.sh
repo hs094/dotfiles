fcb484f2a2e3a7e4aa4318cd5409893b8d9ac741
hs094 <iamhardikat11@gmail.com>
2025-02-13 17:37:15 +0530

    Corrected nvm dependency error

--- zsh/export.sh
+++ zsh/export.sh
@@ -16,8 +16,8 @@ export PIPENV_VENV_IN_PROJECT=1
 
 # Configure Node Version Manager (NVM)
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 export EDITOR="nvim"
 
