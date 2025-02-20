69b201f58a702f59fcbbcd0f67fd0467499d83a7
hs094 <iamhardikat11@gmail.com>
2025-02-20 11:26:42 +0530

    Added Nvim and Tmux Config

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,12 +1,14 @@
-vim.cmd ("set expandtab") 
-vim.cmd ("set tabstop=2" ) 
-vim.cmd ("set softtabstop=2") 
-vim.cmd ("set shiftwidth=2" ) 
+vim.opt.ignorecase = true
+-- Commands
+vim.cmd ("set expandtab")
+vim.cmd ("set tabstop=2")
+vim.cmd ("set softtabstop=2")
+vim.cmd ("set shiftwidth=2" )
 vim.g.mapleader = " "
 -- Customize line number colors
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#A6ADC8" })         -- Default line numbers
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#F5E0DC" })  -- Highlighted current line number
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2D3149" })  -- Background color for the current line
+vim.api.nvim_set_hl(1, "LineNr", { fg = "#A6ADC8" })         -- Default line numbers
+vim.api.nvim_set_hl(1, "CursorLineNr", { fg = "#F5E0DC" })  -- Highlighted current line number
+vim.api.nvim_set_hl(1, "CursorLine", { bg = "#2D3149" })  -- Background color for the current line
 
 vim.api.nvim_create_autocmd({"InsertEnter", "InsertLeave"}, {
     callback = function()
