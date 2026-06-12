cf0d92896c720b791936dcb309a99e3206cc1c15
hs094 <iamhardikat11@gmail.com>
2026-06-12 19:35:23 +0530

    refactor(nvim): refactor module

--- nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Core keymaps
-vim.keymap.set('n', '<leader>L', function() vim.cmd('Lazy') end, { desc = 'Open/Close Lazy plugin manager' })
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
-vim.keymap.set('n', '<leader>yy', '"+yy', { desc = 'Copy line to system clipboard' })
--- Note: <leader>. is mapped to Snacks.scratch() in snacks.lua
--- Note: <leader>d* DAP mappings live in lua/plugins/dap.lua
--- To clear search highlights, press <Esc> (works by default) or use :noh
