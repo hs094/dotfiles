f249fa80f1e9370722d0cdea03ad16e0b019fc74
hs094 <iamhardikat11@gmail.com>
2026-07-09 11:34:24 +0530

    Updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,19 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+vim.keymap.set("n", "<leader>y", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy relative path to clipboard" })
+
+vim.keymap.set("n", "<leader>Y", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Copy absolute path to clipboard" })
+
+vim.api.nvim_create_user_command("CopyRelPath", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+end, { desc = "Copy relative path of current file to clipboard" })
+
+vim.api.nvim_create_user_command("CopyAbsPath", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, { desc = "Copy absolute path of current file to clipboard" })
